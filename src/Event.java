345c77a4941b20930e8c55070b492b7d2e3184d4
mac0321 13 <mac0321.13@metralha.cec.ime.usp.br>
2016-04-14 09:37:55 -0300

    commit 1

--- src/Event.java
+++ src/Event.java
@@ -11,3 +11,31 @@ public abstract class Event {
 	abstract public void action();
 	abstract public String description();
 }
+
+class EventSet {
+	private Event[] events = new Event[100];
+	private int index = 0;
+	private int next = 0;
+	public void add (Event e) {
+		if(index >= events.length)
+			return;
+		events[index++] = e;
+	}
+	public Event getNext() {
+		boolean looped = false;
+		int start = next;
+		do {
+			next = (next + 1) % events.length;
+			//See if it has looped to the beginning:
+		if (start == next) looped = true;
+		// If it loops past start the list 
+		// is empty	:
+		if ((next == (start + 1) % events.length) && looped)
+			return null;
+		}while (events[next] == null);
+	return events[next];
+	}
+	public void removeCurrent() {
+		events[next] = null;
+	}
+}
